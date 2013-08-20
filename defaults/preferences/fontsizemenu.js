263f1570afe17bd6bacbf3ddb8ce29f56d1ad2a0
YUKI Hiroshi <shimoda@clear-code.com>
2013-08-20 14:09:53 +0900

    Implement "font size" menu

--- defaults/preferences/fontsizemenu.js
+++ defaults/preferences/fontsizemenu.js
@@ -2,4 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// pref("extensions.fontsizemenu@clear-code.com.", );
+pref("extensions.fontsizemenu@clear-code.com.large.size",  16);
+pref("extensions.fontsizemenu@clear-code.com.medium.size", 12);
+pref("extensions.fontsizemenu@clear-code.com.small.size",  10);
